2b28f1b101488bd4105db858b2b6243a9a1690c3
schafickMol <chafmolina17@gmail.com>
2024-02-25 20:29:55 -0600

    MunipalServiceModel

--- AlcaldiaAppDemo/AlcaldiaApp/Models/MunicipalServiceModel.cs
+++ AlcaldiaAppDemo/AlcaldiaApp/Models/MunicipalServiceModel.cs
@@ -1,13 +1,18 @@
-﻿namespace AlcaldiaApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlcaldiaApp.Models
 {
     public class MunicipalServiceModel
     {
         public int Id { get; set; }
 
+        [Display(Name = "Nombre del servicio")]
         public string ServiceName { get; set; }
 
+        [Display(Name = "Descripcion")]
         public string Description { get; set; }
 
+        [Display(Name = "Costo")]
         public double Cost { get; set; }
     }
 }
