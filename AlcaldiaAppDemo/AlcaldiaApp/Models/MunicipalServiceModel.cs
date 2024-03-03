97c8864f3627be5e1a743b8abc10e86e70e46d8f
KevinU20221275 <U20221275@univo.edu.sv>
2024-03-02 23:30:24 -0600

    fix merge conflict

--- AlcaldiaAppDemo/AlcaldiaApp/Models/MunicipalServiceModel.cs
+++ AlcaldiaAppDemo/AlcaldiaApp/Models/MunicipalServiceModel.cs
@@ -6,15 +6,6 @@ namespace AlcaldiaApp.Models
     {
         public int Id { get; set; }
 
-<<<<<<< HEAD
-        [Display (Name = "Nombre del Servicio")]
-        public string ServiceName { get; set; }
-
-        [Display (Name = "Descripcion")]
-        public string Description { get; set; }
-
-        [Display (Name = "Costo")]
-=======
         [Display(Name = "Nombre del servicio")]
         public string ServiceName { get; set; }
 
@@ -22,7 +13,6 @@ namespace AlcaldiaApp.Models
         public string Description { get; set; }
 
         [Display(Name = "Costo")]
->>>>>>> d128b33e7bbc75fc55fdc6fee633c06c15ebf477
         public double Cost { get; set; }
     }
 }
